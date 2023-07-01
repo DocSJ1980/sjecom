ff961ed2660daa67b463412f7d1a5974ad83604c
DocSJ1980 <docsaj@outlook.com>
2023-07-01 15:09:46 +0500

    Create store modal created

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,23 @@
 "use client"
 
 import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
 import { UserButton } from "@clerk/nextjs";
+import { useEffect } from "react";
 
 export default function SetupPage() {
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen = useStoreModal((state) => state.isOpen)
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen()
+    }
+  }, [])
+
   return (
     <div className='p-4'>
-      <Modal title="Test" description="Test Description" isOpen onClose={() => { }}>
-        Children
-      </Modal>
+      Root Page
       {/* <UserButton afterSignOutUrl="/" /> */}
     </div>)
 }
